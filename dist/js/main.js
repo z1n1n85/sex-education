feaa251500e593661c12651976caf9e666a64f44
Zinin Ilya <metal-fox8@yandex.ru>
2023-01-21 18:16:57 +0300

    сборка сайта

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,41 +1,68 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/blocks/modules/footer/footer.js":
-/*!*********************************************!*\
-  !*** ./src/blocks/modules/footer/footer.js ***!
-  \*********************************************/
+/***/ "./src/blocks/components/bg-paralax-sky/bg-paralax-sky.js":
+/*!****************************************************************!*\
+  !*** ./src/blocks/components/bg-paralax-sky/bg-paralax-sky.js ***!
+  \****************************************************************/
 /***/ (function() {
 
+"use strict";
 
 
-/***/ }),
-
-/***/ "./src/blocks/modules/header/header.js":
-/*!*********************************************!*\
-  !*** ./src/blocks/modules/header/header.js ***!
-  \*********************************************/
-/***/ (function() {
-
-modules.define('header', ['i-bem-dom'], function (provide, bemDom) {
-  provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-      js: {
-        inited: function inited() {}
+window.onload = function () {
+  var bg_paralax = document.querySelectorAll('.bg-paralax-sky');
+  var main_paralax = document.querySelector('.main-paralax');
+  if (main_paralax) {
+    var setMouseParalaxStyle = function setMouseParalaxStyle() {
+      var distX = coordXprocent - positionX;
+      var distY = coordYprocent - positionY;
+      positionX += distX * speed;
+      positionY += distY * speed;
+      for (var i = 0; i < bg_paralax.length; i += 1) {
+        var bg_paralax_elem = bg_paralax[i];
+        console.log(bg_paralax_elem);
+        bg_paralax_elem.style.backgroundPosition = "bottom ".concat(50 - positionY / bg_coleff * 10, "% right ").concat(50 - positionX / bg_coleff, "%");
       }
-    }
-  }));
-});
+      requestAnimationFrame(setMouseParalaxStyle);
+    };
+    var bg_coleff = 15;
+    var speed = 0.1;
+    var positionX = 0;
+    var positionY = 0;
+    var coordXprocent = 0;
+    var coordYprocent = 0;
+    setMouseParalaxStyle();
+    main_paralax.addEventListener('mousemove', function (e) {
+      var paralaxWidth = main_paralax.offsetWidth;
+      var paralaxHeight = main_paralax.offsetHeight;
+      var coordX = e.pageX - paralaxWidth / 2;
+      var coordY = e.pageY - paralaxHeight / 2;
+      coordXprocent = coordX / paralaxWidth * 100;
+      coordYprocent = coordY / paralaxHeight * 100;
+    });
+  }
+};
 
 /***/ }),
 
-/***/ "./src/blocks/modules/title-screen/title-screen.js":
-/*!*********************************************************!*\
-  !*** ./src/blocks/modules/title-screen/title-screen.js ***!
-  \*********************************************************/
+/***/ "./src/blocks/modules/table-of-contents/table-of-contents.js":
+/*!*******************************************************************!*\
+  !*** ./src/blocks/modules/table-of-contents/table-of-contents.js ***!
+  \*******************************************************************/
 /***/ (function() {
 
-
+var table_dropdown_btn = document.querySelector('.table-of-contents__dropdown-btn');
+var table_chapter_container = document.querySelector('.table-of-contents__chapter-container');
+table_dropdown_btn.addEventListener("click", function () {
+  if (table_chapter_container.classList.contains('table-of-contents__chapter-container--open')) {
+    table_chapter_container.classList.remove('table-of-contents__chapter-container--open');
+    table_dropdown_btn.textContent = '↓↓↓';
+  } else {
+    table_chapter_container.classList.add('table-of-contents__chapter-container--open');
+    table_dropdown_btn.textContent = '↑↑↑';
+  }
+});
 
 /***/ }),
 
@@ -43,8 +70,12 @@ modules.define('header', ['i-bem-dom'], function (provide, bemDom) {
 /*!*************************************!*\
   !*** ./src/js/import/components.js ***!
   \*************************************/
-/***/ (function() {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _blocks_components_bg_paralax_sky_bg_paralax_sky__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../blocks/components/bg-paralax-sky/bg-paralax-sky */ "./src/blocks/components/bg-paralax-sky/bg-paralax-sky.js");
+/* harmony import */ var _blocks_components_bg_paralax_sky_bg_paralax_sky__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_blocks_components_bg_paralax_sky_bg_paralax_sky__WEBPACK_IMPORTED_MODULE_0__);
 
 
 /***/ }),
@@ -57,14 +88,11 @@ modules.define('header', ['i-bem-dom'], function (provide, bemDom) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_header_header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! %modules%/header/header */ "./src/blocks/modules/header/header.js");
-/* harmony import */ var _modules_header_header__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_modules_header_header__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _modules_title_screen_title_screen__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! %modules%/title-screen/title-screen */ "./src/blocks/modules/title-screen/title-screen.js");
-/* harmony import */ var _modules_title_screen_title_screen__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_title_screen_title_screen__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! %modules%/footer/footer */ "./src/blocks/modules/footer/footer.js");
-/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_modules_footer_footer__WEBPACK_IMPORTED_MODULE_2__);
-
-
+/* harmony import */ var _modules_table_of_contents_table_of_contents__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! %modules%/table-of-contents/table-of-contents */ "./src/blocks/modules/table-of-contents/table-of-contents.js");
+/* harmony import */ var _modules_table_of_contents_table_of_contents__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_modules_table_of_contents_table_of_contents__WEBPACK_IMPORTED_MODULE_0__);
+// import "%modules%/header/header";
+// import "%modules%/title-screen/title-screen";
+// import "%modules%/footer/footer";
 
 
 /***/ })
@@ -147,7 +175,6 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _import_modules__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./import/modules */ "./src/js/import/modules.js");
 /* harmony import */ var _import_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./import/components */ "./src/js/import/components.js");
-/* harmony import */ var _import_components__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_import_components__WEBPACK_IMPORTED_MODULE_1__);
 
 
 }();
