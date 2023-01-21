15b07cba2a300098de3fcac3e0e1cf5b915c1765
Zinin Ilya <metal-fox8@yandex.ru>
2023-01-21 18:29:34 +0300

    подключил js модулей

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
-import "%modules%/header/header";
-import "%modules%/title-screen/title-screen";
-import "%modules%/footer/footer";
+// import "%modules%/header/header";
+// import "%modules%/title-screen/title-screen";
+// import "%modules%/footer/footer";
+import "%modules%/table-of-contents/table-of-contents";
