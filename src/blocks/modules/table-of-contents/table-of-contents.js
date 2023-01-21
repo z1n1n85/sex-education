fff5ace15b86e002b4b3f5383b741eb88b4437ea
Zinin Ilya <metal-fox8@yandex.ru>
2023-01-21 18:26:46 +0300

    сделал раскрывающийся список у table-of-contents

--- src/blocks/modules/table-of-contents/table-of-contents.js
+++ src/blocks/modules/table-of-contents/table-of-contents.js
@@ -1,13 +1,12 @@
-modules.define('table-of-contents', ['i-bem-dom'], function(provide, bemDom) {
+const table_dropdown_btn = document.querySelector('.table-of-contents__dropdown-btn');
+const table_chapter_container = document.querySelector('.table-of-contents__chapter-container');
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
+table_dropdown_btn.addEventListener("click", function (){
+    if(table_chapter_container.classList.contains('table-of-contents__chapter-container--open')){
+        table_chapter_container.classList.remove('table-of-contents__chapter-container--open');
+        table_dropdown_btn.textContent = '↓↓↓';
+    }else {
+        table_chapter_container.classList.add('table-of-contents__chapter-container--open');
+        table_dropdown_btn.textContent = '↑↑↑';
     }
-}));
-
-});
+})
